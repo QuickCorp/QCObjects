1fa8ae29bb1776bee130eb0f7a029ade6ef01a8a
Quickcorp <info@quickcorp.cl>
2019-04-14 19:00:44 -0400

    fix

--- QCObjects.js
+++ QCObjects.js
@@ -805,6 +805,9 @@
         for (var r=0;r<rc.routingSelected.length;r++){
           var routing = rc.routingSelected[r];
           rc.templateURI = '{{COMPONENTS_BASE_PATH}}{{COMPONENT_NAME}}.{{TPLEXTENSION}}'.replace('{{COMPONENT_NAME}}',routing.name.toString()).replace('{{COMPONENTS_BASE_PATH}}',CONFIG.get('componentsBasePath')).replace('{{TPLEXTENSION}}',rc.tplextension);
+          if (!GLOBAL.get('routingPaths')){
+            GLOBAL.set('routingPaths',[]);
+          }
           GLOBAL.get('routingPaths').push(rc.routingPath);
         }
         if (rc.routingSelected.length>0){
