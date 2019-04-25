38427450283741dc378c6774e884c6a6d3937d37
Quickcorp <info@quickcorp.cl>
2019-04-25 15:53:21 -0400

    Adding Effect class

--- QCObjects.js
+++ QCObjects.js
@@ -1358,6 +1358,29 @@
     }
   });
 
+  Class('Effect',{
+    animate: function ({timing, draw, duration}) {
+
+      let start = performance.now();
+
+      requestAnimationFrame(function animate(time) {
+        // timeFraction goes from 0 to 1
+        let timeFraction = (time - start) / duration;
+        if (timeFraction > 1) timeFraction = 1;
+
+        // calculate the current animation state
+        let progress = timing(timeFraction);
+
+        draw(Math.round(progress*100)); // draw it
+
+        if (timeFraction < 1) {
+          requestAnimationFrame(animate);
+        }
+
+      });
+    }
+  });  
+
 	/**
 	* Load every component tag declared in the body
 	**/
