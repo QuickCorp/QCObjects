4621f35440e5fbc9211132092f68241aed96f473
Quickcorp <info@quickcorp.cl>
2019-05-05 17:28:58 -0400

    fullscreen call for components

--- QCObjects.js
+++ QCObjects.js
@@ -928,6 +928,29 @@
         rc._reroute_();
       }
     },
+    fullscreen: function(){
+      var elem = this.body;
+      if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+      } else if (elem.mozRequestFullScreen) { /* Firefox */
+        elem.mozRequestFullScreen();
+      } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+        elem.webkitRequestFullscreen();
+      } else if (elem.msRequestFullscreen) { /* IE/Edge */
+        elem.msRequestFullscreen();
+      }
+    },
+    closefullscreen: function (){
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+      } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+      } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+      }
+    },
     _new_:function (properties){
       this.__new__(properties);
       this.routingWay = CONFIG.get('routingWay');
