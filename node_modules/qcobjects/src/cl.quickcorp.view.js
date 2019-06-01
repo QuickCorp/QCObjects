8b8836a1f53a0bf5ef543b086f61a1b658afc3c8
Quickcorp <info@quickcorp.cl>
2019-06-01 03:24:34 -0400

    update dependencies

---
+++ node_modules/qcobjects/src/cl.quickcorp.view.js
@@ -0,0 +1,14 @@
+Package('cl.quickcorp.model',[
+	Class('MainContainer',HTMLCanvasElement,{
+		__new__:function (){
+			//TODO: Implement 
+			console.log('Main Canvas Initialized');
+		}
+	}),
+	Class('MainView',HTMLDivElement,{
+		__new__:function (){
+			//TODO: Implement 
+			console.log('Main Canvas Initialized');
+		}
+	}),
+]);
