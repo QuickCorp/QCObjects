e572a29ff89f29166482405a2f023c30ab265631
Quickcorp <info@quickcorp.cl>
2019-06-08 01:53:32 -0400

    fix templateURI

--- src/cl.quickcorp.components.js
+++ src/cl.quickcorp.components.js
@@ -12,7 +12,7 @@ Package('cl.quickcorp.components',[
       _super_('Component','_new_').call(this,o);
       this.tplsource='default';
       this.templateURI=ComponentURI({
-        'COMPONENTS_BASE_PATH':Component.basePath,
+        'COMPONENTS_BASE_PATH':'',
         'COMPONENT_NAME':'README',
         'TPLEXTENSION':'md',
         'TPL_SOURCE':'default' //here is always default in order to get the right uri
