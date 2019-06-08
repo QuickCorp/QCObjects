f1fb4fc26ea0d41d01389a42c58bb1591d5f5a6e
Quickcorp <info@quickcorp.cl>
2019-06-08 18:13:00 -0400

    fade to 1200 miliseconds

--- src/cl.quickcorp.effects.js
+++ src/cl.quickcorp.effects.js
@@ -1,7 +1,7 @@
 'use strict';
 Package('cl.quickcorp.effects',[
   Class('MainTransitionEffect',Effect,{
-    duration:700,
+    duration:1200,
     defaultParams:{
       alphaFrom:0,
       alphaTo:1
