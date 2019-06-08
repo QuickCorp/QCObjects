e9a84533e7cb58418c08c688d542cea4931d74a4
Quickcorp <info@quickcorp.cl>
2019-06-08 01:40:37 -0400

    load README.md

--- src/cl.quickcorp.components.js
+++ src/cl.quickcorp.components.js
@@ -2,14 +2,16 @@
 Package('cl.quickcorp.components',[
   Class('MarkdownComponent',Component,{
     name:'markdowncomponent',
-    templateURI:'../../README.md',
-    tplsource:'default',
+    url:'README.md',
+    tplsource:'none',
     cached:false,
     controller:null,
     view:null,
     templateHandler: 'MarkdownTemplateHandler',
     _new_:function (o){
       _super_('Component','_new_').call(this,o);
+      this.tplsource='default';
+      this.rebuild();
     }
   })
 ]);
