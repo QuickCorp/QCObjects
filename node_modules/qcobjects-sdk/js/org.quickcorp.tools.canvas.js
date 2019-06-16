863fcd34a1200a2b001e719978eba65e8309cf28
Quickcorp <info@quickcorp.cl>
2019-06-16 01:11:12 -0400

    updating sdk version

--- node_modules/qcobjects-sdk/js/org.quickcorp.tools.canvas.js
+++ node_modules/qcobjects-sdk/js/org.quickcorp.tools.canvas.js
@@ -26,22 +26,23 @@
 (function() {
   Package('org.quickcorp.tools.canvas',[
     Class('CanvasTool',{
-      drawImageFilled: function (img,canvas){
+      drawImageFilled: function (img,canvas,zoom=1,px=0,py=0){
         // get the scale
         var scale = Math.max(canvas.width / img.width, canvas.height / img.height);
+        scale = scale*zoom;
         // get the top left position of the image
         var x = (canvas.width / 2) - (img.width / 2) * scale;
         var y = (canvas.height / 2) - (img.height / 2) * scale;
         var ctx = canvas.getContext('2d');
-        ctx.drawImage(img, x, y, img.width * scale, img.height * scale);
+        ctx.drawImage(img, (px+x), (py+y), img.width * scale, img.height * scale);
       },
-      getImageResized: function (img,width,height,resizedImage){
+      getImageResized: function (img,width,height,resizedImage,zoom=1,px=0,py=0){
         var canvas = document.createElement('canvas');
         canvas.width = width;
         canvas.height = height;
         canvas.style.width = width;
         canvas.style.height = height;
-        this.drawImageFilled(img,canvas);
+        this.drawImageFilled(img,canvas,zoom,px,py);
         resizedImage.src = canvas.toDataURL("image/png");
         return canvas;
       }
