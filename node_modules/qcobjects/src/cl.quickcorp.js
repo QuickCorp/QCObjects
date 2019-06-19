8f5cf59ac64a2d4b8bb5acde9b66aef8d3ffa10f
Quickcorp <info@quickcorp.cl>
2019-06-19 14:14:32 -0400

    Removed node_module folder

--- node_modules/qcobjects/src/cl.quickcorp.js
+++
@@ -1,9 +0,0 @@
-Import ('cl.quickcorp.effects');
-Import ('cl.quickcorp.tools');
-Import ('cl.quickcorp.model');
-Import ('cl.quickcorp.components');
-Import ('cl.quickcorp.view');
-Import ('cl.quickcorp.controller');
-
-Package('cl.quickcorp',[
-]);
