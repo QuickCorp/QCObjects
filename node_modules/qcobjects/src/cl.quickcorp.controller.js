efc5b228c78b1ac61ab91e239862de6985d39588
Quickcorp <info@quickcorp.cl>
2019-06-19 14:14:32 -0400

    Removed node_module folder

--- node_modules/qcobjects/src/cl.quickcorp.controller.js
+++
@@ -1,8 +0,0 @@
-Package('cl.quickcorp.model',[
-	Class('MainController',Element,{
-		__new__:function (){
-			//TODO: Implement 
-			console.log('MainController Element Initialized');
-		}
-	}),
-]);
