669a743e4b3bdb810fdd5bbe99f525cac981f638
Quickcorp <info@quickcorp.cl>
2019-06-20 00:49:09 -0400

    pwa

--- src/cl.quickcorp.controller.js
+++ src/cl.quickcorp.controller.js
@@ -1,7 +1,18 @@
 Package('cl.quickcorp.model',[
+	Class('PWAController',Object,{
+		component:null,
+		_new_:function (o){
+			logger.debug('PWAController Element Initialized');
+			this.component = o.component;
+		},
+		done: function (){
+			document.head.innerHTML+=this.component.body.innerHTML;
+			this.component.body.innerHTML="";
+		}
+	}),	
 	Class('MainController',Element,{
 		__new__:function (){
-			//TODO: Implement 
+			//TODO: Implement
 			console.log('MainController Element Initialized');
 		}
 	}),
