3a13ca95cf6eabdbb92aa7b37beb70ccb9098ccf
Quickcorp <info@quickcorp.cl>
2019-06-25 21:34:53 -0400

    fixing style doc

--- src/cl.quickcorp.tools.js
+++ src/cl.quickcorp.tools.js
@@ -1,12 +1,10 @@
 'use strict';
 Package('cl.quickcorp.tools',[
   Class('MarkdownTemplateHandler',DefaultTemplateHandler,{
-    template:'',
     assign:function (data){
       var converter = new showdown.Converter();
       converter.setFlavor('github');
-      var parsedAssignmentText = converter.makeHtml(this.template);
-      return parsedAssignmentText;
+      return converter.makeHtml(this.template);
     }
   })
 ]);
