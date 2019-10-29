5fc2c718426104b6dc1ee32978a3fa9c0fb2095a
Quickcorp <info@quickcorp.cl>
2019-10-29 04:05:25 -0300

    sw version 0.0.16

--- sw.js
+++ sw.js
@@ -23,7 +23,7 @@
  * license document, but changing it is not allowed.
 */
 "use strict";
-const version = "0.0.15";
+const version = "0.0.16";
 const appName = "QCObjects";
 const cacheName = `qcobjects-app-${appName}-${version}`;
 caches.delete(cacheName);
