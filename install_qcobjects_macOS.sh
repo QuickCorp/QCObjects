982f6cafc0e34a0f5e99377caaf32b162a65df56
Jean Machuca <jeanmachuca@users.noreply.github.com>
2020-02-13 02:32:39 -0300

    install_qcobjects_macOS.sh
    official script to install QCObjects on latest macOS

---
+++ install_qcobjects_macOS.sh
@@ -0,0 +1,25 @@
+#!/bin/zsh
+# QCObjects macOS Installer Script
+# ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+#Created On: February, 12, 2020
+#@author: Jean Machuca <correojean@gmail.com> @jeanmachuca
+#
+# QuickCorp/QCObjects is licensed under the
+# GNU Lesser General Public License v3.0
+# [LICENSE] (https://github.com/QuickCorp/QCObjects/blob/master/LICENSE.txt)
+#
+# Permissions of this copyleft license are conditioned on making available
+# complete source code of licensed works and modifications under the same
+# license or the GNU GPLv3. Copyright and license notices must be preserved.
+# Contributors provide an express grant of patent rights. However, a larger
+# work using the licensed work through interfaces provided by the licensed
+# work may be distributed under different terms and without source code for
+# the larger work.
+#
+# Copyright (C) 2015 Jean Machuca,<correojean@gmail.com>
+#
+# Everyone is permitted to copy and distribute verbatim copies of this
+# license document, but changing it is not allowed.#
+#
+#global
+curl -L https://qcobjects.dev/bin/install/macOS/install_qcobjects.sh | sudo zsh
