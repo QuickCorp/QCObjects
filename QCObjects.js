527a9e9ad7f3507e2786b8a923785bc2363ab90f
Jean Machuca <correojean@gmail.com>
2021-08-15 21:22:51 -0400

    add NamespaceRef() function to allow represent a package namespace in a tree

--- QCObjects.js
+++ QCObjects.js
@@ -81,7 +81,8 @@
         "matrix3d",
         "unique",
         "uniqueId",
-        "shortCode"
+        "shortCode",
+        "NamespaceRef"
       ];
       var _ret_;
       if (_protected_symbols.includes(this.name)) {
@@ -1284,6 +1285,18 @@
   };
   Package.prototype.toString = function() {
     return "Package(namespace, classes) { [QCObjects native code] }";
+  };
+
+  /**
+  * Declare Namespace
+  *
+  * @param {String} packageName
+  * @param {Object} package
+  */
+  var NamespaceRef = function (namespace){
+    let package = Package(namespace);
+    let classes = package.filter(c=>isQCObjects_Class(c)).map(c=>{return {[c.__definition.__classType]:c}}).reduce ((a, b)=> Object.assign(a, b));
+    return namespace.split(".").map(c=>{return {[c]:classes}}).reverse().reduce ( (a, b) => {b[Object.keys(b)]=a;return b;} );
   };
 
 
@@ -2854,6 +2867,7 @@
   Export(_DataStringify);
   Export(isQCObjects_Class);
   Export(isQCObjects_Object);
+  Export(NamespaceRef);
 
   asyncLoad(function() {
 
