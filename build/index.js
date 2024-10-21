d07d8af1654d841b48fedb854a8c745b881fc43d
Jean Machuca <jeanmachuca@users.noreply.github.com>
2024-10-20 21:16:04 -0300

    Revert "ensure build process"

--- build/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const QCObjects_1 = __importDefault(require("./QCObjects"));
-exports.default = QCObjects_1.default;
--- public/QCObjects.js
+++
@@ -1,2 +0,0 @@
-import{b as a}from"./chunks/chunk-O2PFFZVJ.js";export default a();
-//# sourceMappingURL=QCObjects.js.map
