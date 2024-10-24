cce852df2d1f410bc638a81cff621876d0a9dd78
Jean Machuca <correojean@gmail.com>
2024-10-24 00:15:36 -0300

    _super_

--- src/QCObjects.ts
+++ src/QCObjects.ts
@@ -195,21 +195,6 @@ import { Class } from "./Class";
       };
     }
 
-    /**
-     * Returns a method from a superior QCObjects Class
-     * It is useful for Class Inheritance in the _new_ and __new__ method constructors
-     * @example _super_('MySuperClass','MySuperMethod').call(this,params) #where this is the current instance and params are method parameters
-     *
-     * @param {String} className
-     * @param {String} classMethodName
-     * @param {Object} params
-     */
-    var _super_ = function (className, classMethodName, params) {
-      return ClassFactory(className)[classMethodName];
-    };
-    _super_.prototype.toString = function () {
-      return "_super_(className,classMethodName,params) { [QCObjects native code] }";
-    };
 
     /**
      * Creates an object from a Class definition
---
+++ src/super.ts
@@ -0,0 +1,18 @@
+import { ClassFactory } from "./ClassFactory";
+
+    /**
+     * Returns a method from a superior QCObjects Class
+     * It is useful for Class Inheritance in the _new_ and __new__ method constructors
+     * @example _super_('MySuperClass','MySuperMethod').call(this,params) #where this is the current instance and params are method parameters
+     *
+     * @param {String} className
+     * @param {String} classMethodName
+     * @param {Object} params
+     */
+    export const _super_ = function (className, classMethodName, params) {
+        return ClassFactory(className)[classMethodName];
+      };
+      _super_.prototype.toString = function () {
+        return "_super_(className,classMethodName,params) { [QCObjects native code] }";
+      };
+  
