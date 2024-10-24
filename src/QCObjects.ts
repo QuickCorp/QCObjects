3c7c7d20fd37811cc98154d4035ec83de569fad5
Jean Machuca <correojean@gmail.com>
2024-10-24 02:05:52 -0300

    Timer

--- src/QCObjects.ts
+++ src/QCObjects.ts
@@ -455,45 +455,6 @@ import { NamespaceRef } from "./NamespaceRef";
     (_protected_code_)(ClassFactory("ArrayList").matrix2d);
     (_protected_code_)(ClassFactory("ArrayList").matrix3d);
 
-    Package("com.qcobjects.timing", [
-      class Timer extends ClassFactory("InheritClass") {
-
-        constructor() {
-          super(...arguments);
-        }
-
-        duration = 1000;
-        alive = true;
-        thread({
-          timing,
-          intervalInterceptor,
-          duration
-        }) {
-          var timer = this;
-
-          let start = performance.now();
-
-          requestAnimationFrame(function thread(time) {
-            // timeFraction goes from 0 to 1
-            let elapsed = (time - start);
-            let timeFraction = elapsed / duration;
-            if (timeFraction > 1) timeFraction = 1;
-
-            // calculate the current progress state
-            let progress = timing(timeFraction, elapsed);
-
-            intervalInterceptor(Math.round(progress * 100)); // draw it
-
-            if ((timeFraction < 1 || duration === -1) && timer.alive) {
-              requestAnimationFrame(thread);
-            }
-
-          });
-        }
-
-
-      }
-    ]);
 
     Package("com.qcobjects.tools.essentials", [
       class Toggle extends ClassFactory("InheritClass") {
---
+++ src/Timer.ts
@@ -0,0 +1,45 @@
+import { ClassFactory } from "./ClassFactory";
+import { InheritClass } from "./InheritClass";
+import { Package } from "./Package";
+
+export class Timer extends InheritClass {
+
+    constructor() {
+      super(...arguments);
+    }
+
+    duration = 1000;
+    alive = true;
+    thread({
+      timing,
+      intervalInterceptor,
+      duration
+    }) {
+      var timer = this;
+
+      let start = performance.now();
+
+      requestAnimationFrame(function thread(time) {
+        // timeFraction goes from 0 to 1
+        let elapsed = (time - start);
+        let timeFraction = elapsed / duration;
+        if (timeFraction > 1) timeFraction = 1;
+
+        // calculate the current progress state
+        let progress = timing(timeFraction, elapsed);
+
+        intervalInterceptor(Math.round(progress * 100)); // draw it
+
+        if ((timeFraction < 1 || duration === -1) && timer.alive) {
+          requestAnimationFrame(thread);
+        }
+
+      });
+    }
+
+
+  }
+
+Package("com.qcobjects.timing", [
+    Timer
+  ]);
