2e16ffd1d2a4322271cb1307fb64ee754a3defa6
Jean Machuca <correojean@gmail.com>
2024-10-24 00:43:06 -0300

    NamespaceRef

--- src/QCObjects.ts
+++ src/QCObjects.ts
@@ -212,28 +212,6 @@ import { Processor } from "./Processor";
 
     Package("com.qcobjects", [Processor]);
 
-    /**
-     * Declare Namespace
-     *
-     * @param {String} packageName
-     * @param {Object} package
-     */
-    var NamespaceRef = function (namespace) {
-      let packageInstance = Package(namespace);
-      let classes = packageInstance.filter(c => isQCObjects_Class(c)).map(c => {
-        return {
-          [c.__definition.__classType]: c
-        };
-      }).reduce((a, b) => Object.assign(a, b));
-      return namespace.split(".").map(c => {
-        return {
-          [c]: classes
-        };
-      }).reverse().reduce((a, b) => {
-        b[Object.keys(b)] = a;
-        return b;
-      });
-    };
 
 
     /**
