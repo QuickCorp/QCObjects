8f428ee852e32caaace60c5c2562c39c2246ef22
Jean Machuca <correojean@gmail.com>
2024-10-24 00:15:58 -0300

    New

--- src/QCObjects.ts
+++ src/QCObjects.ts
@@ -196,21 +196,7 @@ import { Class } from "./Class";
     }
 
 
-    /**
-     * Creates an object from a Class definition
-     *
-     * @param {QC_Object} o
-     * @param {Object} args
-     */
 
-    var New = function (__class__, args) {
-      args = (arguments.length > 1) ? (args) : ({});
-      return (typeof __class__ === "undefined") ? (new Object()) : (new __class__(args));
-    };
-
-    New.prototype.toString = function () {
-      return "New(QCObjectsClassName, args) { [QCObjects native code] }";
-    };
 
     if (!isBrowser) {
       var findPackageNodePath = function (packagename) {
