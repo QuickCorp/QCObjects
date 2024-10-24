a99ee5a6b981b90b9f010db3806aa6b83711a236
Jean Machuca <correojean@gmail.com>
2024-10-23 23:22:48 -0300

    remove _Object_Create

--- src/QCObjects.ts
+++ src/QCObjects.ts
@@ -206,48 +206,6 @@ import "./assign";
      * Primary instance ID of all objects
      */
     var __instanceID;
-    // Adaptation of Production steps of ECMA-262, Edition 5, 15.2.3.5
-    // Reference: http://es5.github.io/#x15.2.3.5
-    var _Object_Create = (function () {
-
-      // make a safe reference to Object.prototype.hasOwnProperty
-      var hasOwn = Object.prototype.hasOwnProperty;
-
-      return function (O) {
-        // 1. If Type(O) is not Object or Null throw a TypeError exception.
-        if (typeof O !== "object") {
-          throw TypeError("Object prototype may only be an Object or null. The type is " + typeof (O));
-        }
-
-        // 2. Let obj be the result of creating a new object as if by the
-        //		expression new Object() where Object is the standard built-in
-        //		constructor with that name
-        // 3. Set the [[Prototype]] internal property of obj to O.
-        var QCObjects = function () { };
-        QCObjects.prototype = O;
-        var obj = new QCObjects();
-
-        // Let's not keep a stray reference to O...
-
-        // 4. If the argument Properties is present and not undefined, add
-        //		own properties to obj as if by calling the standard built-in
-        //		function Object.defineProperties with arguments obj and
-        //		Properties.
-        if (arguments.length > 1) {
-          // Object.defineProperties does ToObject on its first argument.
-          var Properties = Object(arguments[1]);
-          for (var prop in Properties) {
-            if (hasOwn.call(Properties, prop)) {
-              obj[prop] = Properties[prop];
-            }
-          }
-        }
-
-        // 5. Return obj
-        return obj;
-      };
-    })();
-
 
     var __is_raw_class__ = function (o_c) {
       return (typeof o_c === "function" && o_c.toString().startsWith("class")) ? (true) : (false);
