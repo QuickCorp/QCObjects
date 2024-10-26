de16e2ce5af198ab6854ae3963baba15321afdf7
Jean Machuca <correojean@gmail.com>
2024-10-25 21:46:54 -0300

    fixing types of Export

--- src/Export.ts
+++ src/Export.ts
@@ -1,6 +1,6 @@
 import { __make_global__ } from "./make_global";
 
-export const Export = function (f) {
+export const Export = function (f:any) {
     return __make_global__(f);
   };
   Export.prototype.toString = function () {
