726bbd1a2462d765060a135089abf03abc3ea499
Jean Machuca <correojean@gmail.com>
2024-10-25 22:53:37 -0300

    fixing types of platform

--- src/platform.ts
+++ src/platform.ts
@@ -1,3 +1,5 @@
+import { cordova } from "types/global";
+
 export const isDeno:boolean = (typeof window !== "undefined" && "Deno" in window);
 export const isBrowser:boolean = (typeof window !== "undefined" && typeof window.self !== "undefined" && window === window.self) && !isDeno;
 export const isNodeCommonJS:boolean = (typeof module !== "undefined") ? (true): (false);
