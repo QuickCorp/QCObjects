fab06656119e7a37a6cda8c3732fe261726f2057
Jean Machuca <correojean@gmail.com>
2024-10-25 22:45:48 -0300

    fixing types of isQCObjects

--- src/isQCObjects.ts
+++ src/isQCObjects.ts
@@ -1,4 +1,4 @@
-export const isQCObjects_Object = function (_) {
+export const isQCObjects_Object = function (_:any):boolean {
     return (typeof _ === "object" &&
       Object.hasOwnProperty.call(_, "__classType") &&
       (!!_.__instanceID) &&
@@ -7,7 +7,7 @@ export const isQCObjects_Object = function (_) {
     ) ? (true) : (false);
   };
 
-  export const isQCObjects_Class = function (_) {
+  export const isQCObjects_Class = function (_:any):boolean {
     return (typeof _ === "function" &&
       (!_.__instanceID) &&
       (!!_.__definition) &&
