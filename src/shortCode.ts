ca7e3fde01f55b96361a7a434141107da99a3631
Jean Machuca <correojean@gmail.com>
2024-10-26 00:10:17 -0300

    fixing types of shortCode

--- src/shortCode.ts
+++ src/shortCode.ts
@@ -1,11 +1,11 @@
-import { ClassFactory } from "./ClassFactory";
+import { _Crypt } from "./Crypt";
 
 export const shortCode = function () {
     var length = 1000;
-    var code1 = ClassFactory("_Crypt").encrypt((Math.random() * length).toString().replace(".", ""), (new Date()).getTime().toString());
-    var code2 = ClassFactory("_Crypt").encrypt((Math.random() * length).toString().replace(".", ""), (new Date((new Date()).getTime() - 1000 * 1000)).getTime().toString());
-    var shortCode = code2.list().map((o1, index) => {
+    var code1 = _Crypt.encrypt((Math.random() * length).toString().replace(".", ""), (new Date()).getTime().toString());
+    var code2 = _Crypt.encrypt((Math.random() * length).toString().replace(".", ""), (new Date((new Date()).getTime() - 1000 * 1000)).getTime().toString());
+    var shortCode = code2.list().map((o1:any, index:any) => {
       return code1.list()[index] === o1 ? null : o1;
-    }).filter(c => c !== null).join("");
+    }).filter((c:any) => c !== null).join("");
     return shortCode;
   };
