83d60519fa1520a0de2a589624d29fbbd00aedd2
Jean Machuca <correojean@gmail.com>
2024-10-25 22:44:15 -0300

    fixing types of is_a

--- src/is_a.ts
+++ src/is_a.ts
@@ -1,11 +1,13 @@
 import { __getType__ } from "./getType";
+import { isQCObjects_Class, isQCObjects_Object } from "./isQCObjects";
+import { ObjectName } from "./ObjectName";
 
     /**
      * Returns if a class or object is from a determinated type
      * @param {Object} object
      * @param {String} typeName
      */
-    export const is_a = function is_a(obj, typeName) {
+    export const is_a = function is_a(obj:any, typeName:string) {
         return (typeof obj !== "undefined" && obj !== null &&
           (((isQCObjects_Class(obj) || isQCObjects_Object(obj)) && (obj.hierarchy().includes(typeName))) ||
             __getType__(obj) === typeName ||
